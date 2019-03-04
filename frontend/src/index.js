33bd564cd549246077feb6dccefe18fddea23e74
AGZain <abdulzainulabedin@gmail.com>
2019-03-03 20:12:51 -0500

    adding docker for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import Login from './Login'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//ReactDOM.render(<Login />, document.getElementById('root'));
+//  ReactDOM.render(<Login />, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
